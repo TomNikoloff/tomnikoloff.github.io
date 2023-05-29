d8c58343bec0b0fe480394234a992d09c9ea69d9
Thomas Nikoloff <thomas@haiku.co.uk>
2023-05-29 20:49:57 +0100

    Switched to html

--- includes/html/core/footer.php
+++
@@ -1,7 +0,0 @@
-        <!--
-        <footer>
-            <h3 class=" text-center footer_Copyrigh">
-                Handcrafted by me <i class="far fa-copyright" aria-hidden="true"></i> twentyTwentyTwo
-            </h3>
-        </footer>
-        -->
