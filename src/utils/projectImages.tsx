ad4270d020d7144ca8698bcc836c7c87814889ae
Thomas Nikoloff <tomnikoloff@hotmail.co.uk>
2025-05-28 19:51:38 +0100

    CRM Example Images Update

--- src/utils/projectImages.tsx
+++ src/utils/projectImages.tsx
@@ -5,6 +5,17 @@ import CRM1 from '../assets/CRM-1.png';
 import CRM2 from '../assets/CRM-2.png';
 import CRM3 from '../assets/CRM-3.png';
 
+// new CRM overview & analytics images
+import CRMAdviserOverview from '../assets/New_CRM_Examples/CRM-Adviser_Overview.png';
+import CRMAnalytics from '../assets/New_CRM_Examples/CRM-Analytics.png';
+import CRMCaseCreation from '../assets/New_CRM_Examples/CRM-Case_Creation.png';
+import CRMCaseOverview from '../assets/New_CRM_Examples/CRM-Case_Overview.png';
+import CRMFactfind1 from '../assets/New_CRM_Examples/CRM-Factfind_1.png';
+import CRMFactfind2 from '../assets/New_CRM_Examples/CRM-Factfind_2.png';
+import CRMFactfind3 from '../assets/New_CRM_Examples/CRM-Factfind_3.png';
+import CRMSearch from '../assets/New_CRM_Examples/CRM-Search.png';
+import CRMTransactions from '../assets/New_CRM_Examples/CRM-Transactions.png';
+
 import KataFlow from '../assets/KataFlow.png';
 import KataFlow2 from '../assets/KataFlow-2.png';
 import KataFlow3 from '../assets/KataFlow-3.png';
@@ -25,6 +36,43 @@ const ProjectImage = (image: any) => {
 
         case 'ReactWeatherApp.png':
             Image = ReactWeatherApp;
+            break;
+
+        // new CRM files
+        case 'CRM-Adviser_Overview.png':
+            Image = CRMAdviserOverview;
+            break;
+
+        case 'CRM-Analytics.png':
+            Image = CRMAnalytics;
+            break;
+
+        case 'CRM-Case_Creation.png':
+            Image = CRMCaseCreation;
+            break;
+
+        case 'CRM-Case_Overview.png':
+            Image = CRMCaseOverview;
+            break;
+
+        case 'CRM-Factfind_1.png':
+            Image = CRMFactfind1;
+            break;
+
+        case 'CRM-Factfind_2.png':
+            Image = CRMFactfind2;
+            break;
+
+        case 'CRM-Factfind_3.png':
+            Image = CRMFactfind3;
+            break;
+
+        case 'CRM-Search.png':
+            Image = CRMSearch;
+            break;
+
+        case 'CRM-Transactions.png':
+            Image = CRMTransactions;
             break;
 
         case 'CRM-1.png':
